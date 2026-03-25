f50bc29b47fd99854594787174107ef009c4d149
20148528 <20148528@tafe.wa.edu.au>
2026-03-25 11:35:33 +0800

    Fix paths and dependencies

--- doc/conf.py
+++ doc/conf.py
@@ -3,7 +3,7 @@ author = 'Alice, Bob'
 release = '0.1'
 
 extensions = ["myst_nb", "autodoc2"]
-autodoc2_packages = ["multiply.py"]
+autodoc2_packages = ["../src/multiply.py"]
 
 exclude_patterns = ['_build']
 html_theme = "sphinx_rtd_theme"
